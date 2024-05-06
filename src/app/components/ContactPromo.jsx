ecae5adc45f07a1ae4880fa8e8d6c562103c80d0
jesutobiDev <jesutobi_dev@outlook.com>
2024-05-07 00:00:13 +0100

    css changes

--- src/app/components/ContactPromo.jsx
+++ src/app/components/ContactPromo.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
 const ContactPromo = () => {
   return (
-    <div className='bg-contactpromoBackground w-full h-auto bg-cover text-white  p-7 lg:px-20 lg:py-20 flex flex-col lg:flex-row lg:items-center justify-start gap-20 lg:gap-[200px]' id="contact">
-    <div className="lg:w-1/3 tracking-wider text-xl">Secure the future of your company with <strong>int+</strong></div>
-    <a href='#' className='flex items-center justify-center px-4 py-4 rounded-full bg-opacity-20 bg-white backdrop-filter backdrop-blur-lg border border-opacity-10 border-white border-solid shadow-md text-white w-[50px] h-[50px]'>
-      <Image
-        src="/arrow.svg"
-        alt=''
-        width={25}
-        height={15}
-      />
-    </a>
-  </div>
-  )
-}
+    <div className='bg-contactpromoBackground animate-waves w-full h-auto bg-cover text-white p-7 lg:px-20 lg:py-20 flex flex-col lg:flex-row lg:items-center justify-start gap-20 lg:gap-[200px]' id="contact">
+      <div className="lg:w-1/3 tracking-wider text-xl">Secure the future of your company with <strong>int+</strong></div>
+      <a href='#' className='group flex transition duration-500 ease-in-out items-center gap-7 justify-center px-4 py-4 rounded-full bg-opacity-20 bg-white backdrop-filter backdrop-blur-lg border border-opacity-10 border-white border-solid shadow-md text-white min-w-[50px] h-[50px]'>
+        <span className='hidden group-hover:inline transition ease-in-out'>Get in touch</span>
+        <Image
+          src="/arrow.svg"
+          alt=''
+          width={25}
+          height={15}
+        />
+      </a>
+    </div>
+  );
+};
 
-export default ContactPromo
+export default ContactPromo;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,25 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "homepageBackground" : "url('/homepage-background.svg')",
-        "solutionsBackground" : "url('/solutions.svg')",
-        "contactpromoBackground" : "url('/contactpromo.svg')",
-        "industriesBackground" : "url('/industries.svg')",
-        "Background404" : "url('/404.svg')",
+        "homepageBackground": "url('/homepage-background.svg')",
+        "solutionsBackground": "url('/solutions.svg')",
+        "contactpromoBackground": "url('/contactpromo.svg')",
+        "industriesBackground": "url('/industries.svg')",
+        "Background404": "url('/404.svg')",
       },
-      fontFamily:{
-        calistoga : ['var(--font-calistoga)']
-      }
-    },
-  },
-  plugins: [
-
-],
-};
+      fontFamily: {
+        calistoga: ['var(--font-calistoga)']
+      },
+      animation: {
+        'waves': 'waves 2s linear infinite',
+      },
+      keyframes: {
+        'waves': {
+          '0%': { backgroundPositionX: '0' },
+          '100%': { backgroundPositionX: '1000px' },
+        }
+      }, // Add a comma here
+    }, // Close extend object
+  }, // Close theme object
+  plugins: [], // Close plugins array
+}; // Close module.exports object
