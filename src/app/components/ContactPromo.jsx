0efcb6ef5a8eca81ebd1494cd147a1d6bda25e2e
jesutobiDev <jesutobi_dev@outlook.com>
2024-05-09 10:29:15 +0100

    video additions

--- src/app/components/ContactPromo.jsx
+++ src/app/components/ContactPromo.jsx
@@ -3,13 +3,13 @@ import Image from 'next/image'
 
 const ContactPromo = () => {
   return (
-    <div className='relative w-full h-[500px] lg:h-auto bg-black bg-cover text-white p-7 lg:px-20 lg:py-20 flex flex-col lg:flex-row lg:items-center justify-start gap-20 lg:gap-[200px]'>
+    <div className='relative w-full h-auto bg-black bg-cover text-white p-7 lg:px-20 lg:py-20 flex flex-col lg:flex-row lg:items-center justify-start gap-20 lg:gap-[200px]'>
             <video autoPlay loop muted className='absolute inset-0 w-full h-full object-cover'>
         <source src="/contactpromo-video.mp4" type="video/mp4" />
         Your browser does not support the video tag...
       </video>
       <div className="lg:w-1/3 tracking-wider text-xl z-10">Secure the future of your company with  <span className='font-semibold'>int+</span></div>
-      <a href='#' className=' group flex gap-10 items-center justify-between px-4 py-4 rounded-full bg-opacity-20 bg-white backdrop-filter backdrop-blur-lg border border-opacity-10 border-white border-solid shadow-md text-white w-[70px] h-[70px] overflow-hidden hover:w-[300px]  transition-all duration-300 ease-in-out z-10'>
+      <a href='#' className=' group flex gap-10 items-center justify-between px-4 py-4 rounded-full bg-opacity-20 bg-white backdrop-filter backdrop-blur-lg border border-opacity-10 border-white border-solid shadow-md text-white w-[70px] h-[70px] overflow-hidden hover:w-[300px]  transition-all duration-300 ease-in-out z-10lg:'>
 
         <div className='w-[70px] h-[70px] flex items-center justify-center'>
           <Image
