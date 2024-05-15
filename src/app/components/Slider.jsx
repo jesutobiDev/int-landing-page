4b33462d347406bd3ccfa28670d89d6c00586fb5
jesutobiDev <jesutobi_dev@outlook.com>
2024-05-15 17:10:44 +0100

    content changes

--- src/app/components/Slider.jsx
+++ src/app/components/Slider.jsx
@@ -4,7 +4,7 @@ import 'swiper/css';
 import 'swiper/css/bundle';
 import Image from 'next/image';
 import cards from '../deck-data';
-import {EffectCards, Autoplay} from 'swiper';
+import { EffectCards, Autoplay } from 'swiper';
 import 'swiper/css/effect-cards';
 
 const Slider = () => {
@@ -15,20 +15,20 @@ const Slider = () => {
             centeredSlides={true}
             loop={true}
             speed={500}
-            autoplay={{ delay: 10000,
-                 disableOnInteraction: false, 
-                }}
+            autoplay={{
+                delay: 10000,
+                disableOnInteraction: false,
+            }}
             rotate={true}
             cardsEffect={{
                 perSlideOffset: 8,
                 perSlideRotate: 2,
-
             }}
             modules={[EffectCards, Autoplay]}
-            className="mySwiper"
+            className="h-fit"
         >
             {cards.map((card, index) => (
-                <SwiperSlide key={index} className={`${card.background} rounded-2xl p-6`}>
+                <SwiperSlide key={index} className={`${card.background} rounded-2xl p-6 h-full`}>
                     <div className='flex gap-4 items-base'>
                         <Image
                             src={card.icon}
@@ -38,7 +38,7 @@ const Slider = () => {
                         />
                         <p className="font-semibold text-2xl">{card.industry}</p>
                     </div>
-                    <p className='font-normal text-base leading-[25px] my-4'>{card.text}</p>
+                    <p className='font-normal text-base leading-[25px] my-4' dangerouslySetInnerHTML={{ __html: card.text }} />
                 </SwiperSlide>
             ))}
         </Swiper>
